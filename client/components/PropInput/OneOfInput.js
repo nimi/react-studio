ef9449e8cc77c072517b831e927bbeb71956b3a8
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2016-05-01 23:13:45 -0400

    Add OneOfInput functionality

--- client/components/PropInput/OneOfInput.js
+++ client/components/PropInput/OneOfInput.js
@@ -1,19 +1,74 @@
 import React, {Component, PropTypes} from 'react';
 import style from './style.css';
 
-function OneOfInput({prop, options}) {
-	const opts = options.map((opt, i) =>
-		(<option key={i} value={opt}>{opt}</option>));
+import StringInput from './StringInput';
+import NumberInput from './NumberInput';
 
-	return (
-		<div className={style.PropInput}>
-			<label>{prop}</label>
-			<select defaultValue={1}>
-				<option>Choose prop type</option>
-				{opts}
-			</select>
-		</div>
-	);
+class OneOfInput extends Component {
+	static inputMap = new Map([
+		['string', StringInput],
+		['number', NumberInput]
+	]);
+
+	_handleTypeSelected(e) {
+		const {value} = e.target;
+		const {inputMap} = OneOfInput;
+		const {handleTypeChange, prop} = this.props;
+
+		if (inputMap.has(value)) {
+			handleTypeChange({[prop]: value});
+		}
+	}
+
+	_handleInputSelected(value) {
+		const {handleChange} = this.props;
+		handleChange(value);
+	}
+
+	_renderInput(type) {
+		const {inputMap} = OneOfInput;
+		const {handleChange, prop} = this.props;
+
+		if (inputMap.has(type)) {
+			const Component = inputMap.get(type);
+			return (
+				<Component
+					{...this.props}
+					handleChange={::this._handleInputSelected}
+				/>
+			);
+		}
+	}
+
+	_renderOptions() {
+		const {prop, options} = this.props;
+		const opts = options.map((opt, i) => (
+			<option
+				key={i}
+				value={opt}
+			>
+				{opt}
+			</option>
+		));
+
+	}
+
+	render() {
+		const {prop, options, value, selectedType} = this.props;
+		const opts = options.map((opt, i) =>
+			(<option key={i} value={opt}>{opt}</option>));
+
+		return (
+			<div className={`${style.PropInput} ${style.OneOfInput}`}>
+				<label>{prop}</label>
+				<select defaultValue={1} onChange={::this._handleTypeSelected}>
+					<option value=''>Choose prop type</option>
+					{opts}
+				</select>
+				{this._renderInput(selectedType)}
+			</div>
+		);
+	}
 }
 
 export default OneOfInput;
